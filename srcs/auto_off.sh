b8a27db4989cd9d7ca8e4e778bea575af69af816
Aymane ELmadidi <aymanelmadidi@gmail.com>
2021-05-26 15:44:53 +0100

    L3ez Aimad

--- srcs/auto_off.sh
+++ srcs/auto_off.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Run this script inside the container to switch autoindex to off
 cp /tmp/nginx_auto_off.conf /etc/nginx/sites-enabled/default
 mv /var/www/html/.index.html /var/www/html/index.html
 nginx -s reload
--- srcs/auto_on.sh
+++ srcs/auto_on.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Run this script inside the container to switch autoindex to on
 cp /tmp/nginx_auto_on.conf /etc/nginx/sites-enabled/default
 mv /var/www/html/index.html /var/www/html/.index.html
 nginx -s reload
